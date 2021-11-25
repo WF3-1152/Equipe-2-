a92760cf0e1a17ae3f8bb3b596c26916a3d5be3e
Elyas RAJJAF <93983897+elyasrajjaf@users.noreply.github.com>
2021-11-25 14:54:04 +0100

    update css footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,10 +1,11 @@
 <footer class="mt-auto">
-   <div class="d-flex justify-content-center text-white bg-dark">
+   <div class="d-flex justify-content-center text-white" style="background-color: #000;">
        <div class="me-3">
         <img src="assets/oiseaux.jpg" alt="oiseau de laura" height='40px;' width="50px;">
        </div>
        <div>
-       <p>Avec la participation de Laura, Anne, Olivier, Elyas et Romain</p>
+       <p style="text-align: center;">Avec la participation de Laura, Anne, Olivier, Elyas et Romain.</p>
+       <p style="text-align: center; font-size:13px;">&copy;<?=date('Y');?> - Manga Mise</p>
        </div>
        <div class="ms-3">
         <img src="assets/Rosso.jpg" alt="oiseau de laura" height='40px;' width="65px;">
